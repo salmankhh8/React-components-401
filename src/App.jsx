e22567713051da7d43b3b095b3d312cd1e0bdfa1
salmankhh8 <62678675+salmankhh8@users.noreply.github.com>
2021-09-09 23:02:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading"; // importoing fro  jsx
+import List from "./list";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+export default App;
---
+++ src/Heading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Heading() {
+  // function name should strt from capitol(pascal case)
+  return <h1>My Favourite Foods</h1>; // return h1
+}
+
+export default Heading;
